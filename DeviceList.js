427e270cf32937e9926b6a587bde96e010f9ccfe
BKMEN <91731848+BKMEN@users.noreply.github.com>
2025-05-17 23:01:54 +0200

    Create DeviceList.js

---
+++ DeviceList.js
@@ -0,0 +1,10 @@
+export default [
+    // Lista tus dispositivos aquí
+    {
+        id: 'xxx',
+        name: 'Tira LED escritorio',
+        key: 'yyy',
+        leds: 15,
+    },
+    // ...
+];
