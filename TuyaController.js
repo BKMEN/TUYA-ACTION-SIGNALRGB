b3222a3bc29ccf816d0c2b492170f051544a11f7
BKMEN <91731848+BKMEN@users.noreply.github.com>
2025-05-17 23:01:04 +0200

    Create TuyaController.js

---
+++ TuyaController.js
@@ -0,0 +1,11 @@
+export default class TuyaController {
+    constructor(deviceList) {
+        this.deviceList = deviceList;
+        this.devices = [];
+    }
+
+    discoverDevices() {
+        // Aquí va el código para buscar dispositivos Tuya
+        // Por ejemplo, escanear la red, etc.
+    }
+}
