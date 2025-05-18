46ea6f3b87e627538de4828be3d2539c67e836e2
Yovan <yovanjcd@gmail.com>
2025-05-18 23:48:42 +0200

    Refactor index.js to reorganize function definitions for improved clarity and maintainability, including the addition of VendorId and ProductId functions.

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ export function Name() { return "TuyaAction"; }
 export function Publisher() { return "Yovan"; }
 export function Version() { return "0.0.1"; }
 export function Type() { return "network"; }
-export function Size() { return [1, 1]; }
-export function ControllableParameters() {
-    return []; // Puedes dejarlo vacío solo para test
-}
+export function VendorId() { return 0; }
+export function ProductId() { return 0; }
+export function Size() { return [1,1]; }
+export function ControllableParameters() { return []; }
 export function LedNames() { return ["LED 1"]; }
 export function LedPositions() { return [[0, 0]]; }
+export function Render() {}
+export function Shutdown() {}
+export function ImageUrl() { return ""; }
