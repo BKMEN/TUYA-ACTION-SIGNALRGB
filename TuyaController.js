5bceee84d1b2fd7f415fd5a46fafe3affccd7696
Yovan <yovanjcd@gmail.com>
2025-05-18 19:53:12 +0200

    TUYA CONTROLLER

--- TuyaController.js
+++ TuyaController.js
@@ -1,11 +1,33 @@
-export default class TuyaController {
-    constructor(deviceList) {
-        this.deviceList = deviceList;
+// TuyaController.js
+const TuyaDiscovery = require('./comms/Discovery.js');
+
+class TuyaController {
+    constructor() {
         this.devices = [];
     }
 
     discoverDevices() {
-        // Aquí va el código para buscar dispositivos Tuya
-        // Por ejemplo, escanear la red, etc.
+        const discovery = new TuyaDiscovery({ timeout: 6000 });
+        discovery.on('device', (dev) => {
+            console.log('Dispositivo encontrado:', dev);
+            this.devices.push(dev);
+        });
+        discovery.on('done', (list) => {
+            console.log('Búsqueda finalizada, total:', list.length);
+        });
+        discovery.on('error', (err) => {
+            console.error('Discovery error:', err);
+        });
+
+        discovery.discover();
     }
 }
+
+module.exports = TuyaController;
+
+/*
+---- USO ----
+const TuyaController = require('./TuyaController.js');
+const controller = new TuyaController();
+controller.discoverDevices();
+*/
